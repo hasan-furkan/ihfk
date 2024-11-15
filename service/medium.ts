cd4146502a6ded1ccc97137482cd97a2bbd9499c
hasan-furkan <hsnfrkn32@gmail.com>
2024-11-15 01:04:49 +0100

    delete console.log

--- service/medium.ts
+++ service/medium.ts
@@ -1,5 +1,4 @@
 export const getMediumPosts = async () => {
-    console.log(process.env.MEDIUM_RSS_URL);
     const res = await fetch(
         `https://api.rss2json.com/v1/api.json?rss_url=${process.env.MEDIUM_RSS_URL}`
     );
